476f17ab6c428589af53d6799bc94b987c77f120
darken <darken@darken.eu>
2019-11-16 16:30:45 +0100

    Don't need special pkg lists, don't use root to get installed apps.

--- app/src/main/java/eu/darken/bb/common/pkgs/PkgRepo.kt
+++ app/src/main/java/eu/darken/bb/common/pkgs/PkgRepo.kt
@@ -1,39 +1,24 @@
 package eu.darken.bb.common.pkgs
 
 import android.annotation.SuppressLint
-import android.annotation.TargetApi
 import android.content.Context
 import android.content.pm.PackageInfo
 import android.content.pm.PackageManager
-import android.os.Build
 import eu.darken.bb.App
 import eu.darken.bb.common.ApiHelper
 import eu.darken.bb.common.dagger.AppContext
 import eu.darken.bb.common.dagger.PerApp
-import eu.darken.bb.common.root.RootManager
-import eu.darken.rxshell.cmd.Cmd
-import eu.darken.rxshell.cmd.RxCmdShell
-import io.reactivex.schedulers.Schedulers
-import org.w3c.dom.Document
-import org.w3c.dom.Element
-import org.xml.sax.InputSource
 import timber.log.Timber
-import java.io.File
-import java.io.FileReader
 import java.util.*
-import java.util.regex.Pattern
 import javax.inject.Inject
-import javax.xml.parsers.DocumentBuilderFactory
 
 @PerApp
 class PkgRepo @Inject constructor(
         @AppContext private val context: Context,
-        private val rootManager: RootManager,
         private val ipcFunnel: IPCFunnel
 ) {
     private val requestCache = HashMap<PkgRequest, CachedRequest>()
     private val cacheLock = Any()
-    private val pattern = Pattern.compile("^(?:package:)(.+?)(?:=)([\\w._]+)$")
 
     internal class CachedRequest(data: Map<String, Pkg>) {
         private val timestamp: Long = System.currentTimeMillis()
@@ -67,9 +52,6 @@ class PkgRepo @Inject constructor(
                         if (!appMap.containsKey(pkg.packageName)) appMap[pkg.packageName] = AppPkg(pkg)
                     }
 
-                    checkForHiddenPackages(appMap, request)
-                    checkForLibraryPackages(appMap, request)
-
                     cachedRequest = CachedRequest(appMap)
                     requestCache[request] = cachedRequest!!
                 }
@@ -78,117 +60,8 @@ class PkgRepo @Inject constructor(
         return cachedRequest!!.data
     }
 
-    private fun checkForHiddenPackages(appMap: MutableMap<String, Pkg>, request: PkgRequest) {
-        val rootContext = rootManager.rootContext.subscribeOn(Schedulers.io()).blockingGet()
-        val hiddenPackages = ArrayList<InstantPkg>()
-        if (ApiHelper.hasOreo() && rootContext.isRooted) {
-            val result = Cmd.builder("pm list packages -f").execute(RxCmdShell.builder().root(true).build())
-            Timber.tag(TAG).d("Result: %s", result)
-
-            for (s in result.output) {
-                val matcher = pattern.matcher(s)
-                if (matcher.matches()) {
-                    val pkgName = matcher.group(2)
-                    if (appMap.containsKey(pkgName)) continue
-
-                    val sourcePath = matcher.group(1)
-                    val pkgInfo = ipcFunnel.submit(object : IPCFunnel.PMQuery<PackageInfo?> {
-                        override fun onPackManAction(pm: PackageManager): PackageInfo? {
-                            return pm.getPackageArchiveInfo(sourcePath, request.flags)
-                        }
-                    })
-                    if (pkgInfo != null) {
-                        val app = InstantPkg(pkgInfo, sourcePath)
-                        hiddenPackages.add(app)
-                    }
-                }
-            }
-
-        }
-
-        if (hiddenPackages.isNotEmpty()) {
-            val dest = File(context.cacheDir, "packages.xml-" + UUID.randomUUID().toString())
-            try {
-                val cpResult = Cmd.builder(
-                        "cp /data/system/packages.xml " + dest.absolutePath,
-                        "chown -R `stat " + dest.parent + " -c %u:%g` " + dest.absolutePath
-                ).execute(RxCmdShell.builder().root(true).build())
-
-                if (cpResult.exitCode == Cmd.ExitCode.OK) {
-                    val docFactory = DocumentBuilderFactory.newInstance()
-                    val docBuilder = docFactory.newDocumentBuilder()
-
-                    val inputSource = InputSource(FileReader(dest))
-                    val document = docBuilder.parse(inputSource)
-
-                    if (document != null) {
-                        for (instantApp in hiddenPackages) {
-                            updateHiddenPackage(instantApp, document)
-                        }
-                    }
-                }
-
-            } catch (e: Exception) {
-                Timber.tag(TAG).e(e)
-            } finally {
-                val deleted = dest.delete()
-                Timber.tag(TAG).d("Temp file deletion (success=%b, path=%s", deleted, dest)
-            }
-        }
-
-        for (pkgInfo in hiddenPackages) {
-            appMap[pkgInfo.packageName] = pkgInfo
-        }
-    }
-
-
-    private fun updateHiddenPackage(pkgInfo: InstantPkg, document: Document) {
-        val root = document.documentElement ?: return
-
-        val children = root.childNodes
-        for (i in 0 until children.length) {
-            if (children.item(i) == null) continue
-            if (children.item(i).nodeName == "package") {
-                val entry = children.item(i) as Element
-                if (pkgInfo.packageName != entry.getAttribute("name")) continue
-
-                if (entry.hasAttribute("ut")) {
-                    pkgInfo.lastUpdateTime = java.lang.Long.parseLong(entry.getAttribute("ut"), 16)
-                }
-                if (entry.hasAttribute("it")) {
-                    pkgInfo.firstInstallTime = java.lang.Long.parseLong(entry.getAttribute("it"), 16)
-                }
-            }
-        }
-    }
-
-    @TargetApi(Build.VERSION_CODES.O)
-    private fun checkForLibraryPackages(appMap: MutableMap<String, Pkg>, request: PkgRequest) {
-        if (!ApiHelper.hasOreo()) return
-
-        val pm = context.packageManager
-        val libraryApps = ArrayList<LibraryPkg>()
-
-        val libraryInfos = pm.getSharedLibraries(request.flags)
-        for (libInfo in libraryInfos) {
-            if (libInfo.type == 0) {
-                // Built in types like .jars
-                continue
-            }
-            libraryApps.add(LibraryPkg(libInfo))
-        }
-
-        for (app in libraryApps) {
-            if (appMap.containsKey(app.packageName)) {
-                Timber.tag(TAG).w("Skipping duplicate library package %s and %s", app, appMap[app.packageName])
-                continue
-            }
-            appMap[app.packageName] = app
-        }
-    }
-
     companion object {
-        private val TAG = App.logTag("AppRepo")
+        private val TAG = App.logTag("PkgRepo")
     }
 
 }
