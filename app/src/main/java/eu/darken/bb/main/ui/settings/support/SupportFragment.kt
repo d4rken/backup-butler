217e9987b617962e4432444e4ccb6055047053ba
darken <darken@darken.eu>
2019-11-21 15:43:04 +0100

    Added support email

--- app/src/main/java/eu/darken/bb/main/ui/settings/support/SupportFragment.kt
+++ app/src/main/java/eu/darken/bb/main/ui/settings/support/SupportFragment.kt
@@ -1,5 +1,7 @@
 package eu.darken.bb.main.ui.settings.support
 
+import android.os.Bundle
+import android.view.View
 import androidx.annotation.Keep
 import androidx.preference.Preference
 import com.google.android.material.snackbar.Snackbar
@@ -7,7 +9,7 @@ import eu.darken.bb.GeneralSettings
 import eu.darken.bb.R
 import eu.darken.bb.common.ClipboardHelper
 import eu.darken.bb.common.dagger.AutoInject
-import eu.darken.bb.common.debug.InstallId
+import eu.darken.bb.common.observe2
 import eu.darken.bb.common.smart.SmartPreferenceFragment
 import eu.darken.bb.common.vdc.VDCSource
 import eu.darken.bb.common.vdc.vdcs
@@ -24,22 +26,37 @@ class SupportFragment : SmartPreferenceFragment(), AutoInject {
 
     override val settings: GeneralSettings by lazy { generalSettings }
 
-    @Inject lateinit var installId: InstallId
     @Inject lateinit var clipboardHelper: ClipboardHelper
 
     private val installIdPref by lazy { findPreference<Preference>("support.installid")!! }
+    private val supportMailPref by lazy { findPreference<Preference>("support.email.darken")!! }
 
     override fun onPreferencesCreated() {
 
         installIdPref.setOnPreferenceClickListener {
-            val theId = installId.installId.toString()
-            Snackbar.make(requireView(), theId, Snackbar.LENGTH_INDEFINITE)
-                    .setAction(R.string.general_copy_action) {
-                        clipboardHelper.copyToClipboard(theId)
-                    }
-                    .show()
+            vdc.copyInstallID()
             true
         }
+        supportMailPref.setOnPreferenceClickListener {
+            vdc.sendSupportMail()
+            true
+        }
+
         super.onPreferencesCreated()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        vdc.clipboardEvent.observe2(this) { installId ->
+            Snackbar.make(requireView(), installId, Snackbar.LENGTH_INDEFINITE)
+                    .setAction(R.string.general_copy_action) {
+                        clipboardHelper.copyToClipboard(installId)
+                    }
+                    .show()
+        }
+
+        vdc.emailEvent.observe2(this) {
+            startActivity(it)
+        }
+        super.onViewCreated(view, savedInstanceState)
     }
 }
--- app/src/main/java/eu/darken/bb/main/ui/settings/support/SupportFragmentVDC.kt
+++ app/src/main/java/eu/darken/bb/main/ui/settings/support/SupportFragmentVDC.kt
@@ -1,14 +1,58 @@
 package eu.darken.bb.main.ui.settings.support
 
+import android.content.Intent
+import android.os.Build
 import androidx.lifecycle.SavedStateHandle
 import com.squareup.inject.assisted.Assisted
 import com.squareup.inject.assisted.AssistedInject
+import eu.darken.bb.BackupButler
+import eu.darken.bb.common.EmailTool
+import eu.darken.bb.common.SingleLiveEvent
+import eu.darken.bb.common.debug.InstallId
 import eu.darken.bb.common.vdc.SavedStateVDCFactory
 import eu.darken.bb.common.vdc.SmartVDC
+import io.reactivex.Observable
+import io.reactivex.schedulers.Schedulers
 
 class SupportFragmentVDC @AssistedInject constructor(
-        @Assisted private val handle: SavedStateHandle
+        @Assisted private val handle: SavedStateHandle,
+        val installId: InstallId,
+        val backupButler: BackupButler,
+        val emailTool: EmailTool
 ) : SmartVDC() {
+
+    val emailEvent = SingleLiveEvent<Intent>()
+    val clipboardEvent = SingleLiveEvent<String>()
+
+    fun sendSupportMail() {
+        Observable
+                .fromCallable {
+                    val bodyInfo = StringBuilder("\n\n\n")
+
+                    bodyInfo.append("--- Infos for the developer ---\n")
+
+                    val appInfo = backupButler.appInfo
+                    val versionStr = "${appInfo.versionName} (${appInfo.versionCode}) [${appInfo.gitSha}]"
+                    bodyInfo.append("App version: ").append(versionStr).append("\n")
+
+                    bodyInfo.append("Update history: ").append(backupButler.updateHistory).append("\n")
+                    bodyInfo.append("Device: ").append(Build.FINGERPRINT).append("\n")
+                    bodyInfo.append("Install ID: ").append(installId.installId.toString()).append("\n")
+
+                    val email = EmailTool.Email(
+                            receipients = listOf("support@darken.eu"),
+                            subject = "[SD Maid] Question/Suggestion/Request\n",
+                            body = bodyInfo.toString()
+                    )
+                    return@fromCallable emailTool.build(email)
+                }
+                .subscribeOn(Schedulers.io())
+                .subscribe { emailEvent.postValue(it) }
+    }
+
+    fun copyInstallID() {
+        clipboardEvent.postValue(installId.installId.toString())
+    }
 
     @AssistedInject.Factory
     interface Factory : SavedStateVDCFactory<SupportFragmentVDC>
