d203fe6f3abd43eba5fb0cbab21cd14ae1acc0ef
darken <darken@darken.eu>
2021-12-04 22:19:03 +0100

    Remove mode step during onboarding, always start on quick mode.

--- app/src/main/java/eu/darken/bb/onboarding/ui/cost/CostStepFragment.kt
+++ app/src/main/java/eu/darken/bb/onboarding/ui/cost/CostStepFragment.kt
@@ -5,26 +5,21 @@ import android.view.View
 import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 import eu.darken.bb.R
-import eu.darken.bb.common.navigation.doNavigate
-import eu.darken.bb.common.observe2
-import eu.darken.bb.common.smart.SmartFragment
+import eu.darken.bb.common.smart.Smart2Fragment
 import eu.darken.bb.common.viewBinding
 import eu.darken.bb.databinding.OnboardingStepCostFragmentBinding
 import eu.darken.bb.main.core.UISettings
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class CostStepFragment : SmartFragment(R.layout.onboarding_step_cost_fragment) {
+class CostStepFragment : Smart2Fragment(R.layout.onboarding_step_cost_fragment) {
 
-    private val vdc: CostStepFragmentVDC by viewModels()
-    private val ui: OnboardingStepCostFragmentBinding by viewBinding()
+    override val vdc: CostStepFragmentVDC by viewModels()
+    override val ui: OnboardingStepCostFragmentBinding by viewBinding()
 
     @Inject lateinit var uiSettings: UISettings
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-
-        vdc.navEvents.observe2(this) { doNavigate(it) }
-
         ui.continueAction.setOnClickListener { vdc.onContinue() }
 
         super.onViewCreated(view, savedInstanceState)
--- app/src/main/java/eu/darken/bb/onboarding/ui/cost/CostStepFragmentVDC.kt
+++ app/src/main/java/eu/darken/bb/onboarding/ui/cost/CostStepFragmentVDC.kt
@@ -1,21 +1,27 @@
 package eu.darken.bb.onboarding.ui.cost
 
 import androidx.lifecycle.SavedStateHandle
-import androidx.navigation.NavDirections
 import dagger.hilt.android.lifecycle.HiltViewModel
-import eu.darken.bb.common.SingleLiveEvent
-import eu.darken.bb.common.vdc.VDC
+import eu.darken.bb.BackupButler
+import eu.darken.bb.common.coroutine.DispatcherProvider
+import eu.darken.bb.common.smart.Smart2VDC
+import eu.darken.bb.main.core.UISettings
+import eu.darken.bb.onboarding.core.OnboardingSettings
 import javax.inject.Inject
 
 @HiltViewModel
 class CostStepFragmentVDC @Inject constructor(
-    private val handle: SavedStateHandle
-) : VDC() {
-
-    val navEvents = SingleLiveEvent<NavDirections>()
+    private val handle: SavedStateHandle,
+    private val dispatcherProvider: DispatcherProvider,
+    private val onboardingSettings: OnboardingSettings,
+    private val uiSettings: UISettings,
+    private val backupButler: BackupButler,
+) : Smart2VDC(dispatcherProvider) {
 
     fun onContinue() {
-        navEvents.postValue(CostStepFragmentDirections.actionCostStepFragmentToModeStepFragment())
+        onboardingSettings.lastOnboardingVersion = backupButler.appInfo.versionCode
+        uiSettings.startMode = UISettings.StartMode.QUICK
+        navEvents.postValue(CostStepFragmentDirections.actionCostStepFragmentToInitFragment())
     }
 
 }
